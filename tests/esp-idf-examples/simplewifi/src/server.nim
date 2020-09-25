e9f3bdb8f9703f80ed1e6735de9cbb0a4212d4ba
Jaremy Creechley <creechley@gmail.com>
2020-09-25 09:54:31 -0600

    tweaking nim -- use adaptive orc!

--- tests/esp-idf-examples/simplewifi/src/server.nim
+++ tests/esp-idf-examples/simplewifi/src/server.nim
@@ -7,7 +7,7 @@ proc cb*(req: Request) {.async.} =
     inc count
     echo "req #", count
     await req.respond(Http200, "Hello World from nim on ESP32\n")
-    GC_fullCollect()
+    # GC_fullCollect()
 
 proc run_http_server*() {.exportc.} =
     echo "starting http server on port 8181"
