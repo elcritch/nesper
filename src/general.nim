b8d14a5b311cd17fceb0cffd59c6a8f19086ce2c
Jaremy Creechley <creechley@gmail.com>
2020-09-29 00:41:03 -0600

    adding event data

--- src/general.nim
+++ src/general.nim
@@ -19,4 +19,3 @@ proc ESP_ERROR_CHECK_WITHOUT_ABORT*(x: esp_err_t) {.cdecl,
 #define ESP_LOGI( tag, format, ... )  
 #define LOG_FORMAT(letter, format)  LOG_COLOR_ ## letter #letter " (%d) %s: " format LOG_RESET_COLOR "\n"
 
-
