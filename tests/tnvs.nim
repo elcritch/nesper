c397765fe30554b176270b4c9d1b462fe9d4ad29
Jaremy Creechley <creechley@gmail.com>
2020-10-04 13:51:37 -0600

    tnvs values

--- tests/tnvs.nim
+++ tests/tnvs.nim
@@ -3,11 +3,12 @@ import nesper
 import nesper/nvs
 
 
-let nvs_handle = newNvs("storage", NVS_READONLY)
-
+let nvs_handle = newNvs("storage", NVS_READWRITE)
 nvs_handle.setInt("a", 1)
-
 let a = nvs_handle.getInt("a")
-
 echo "a: " & $a
+
+nvs_handle.setStr("b", "bb")
+let b = nvs_handle.getStr("b")
+echo "b: " & $b
 
