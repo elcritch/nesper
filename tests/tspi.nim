8a73a312ebbbe9968f2999f788a9c66e95d0b2f8
Jaremy Creechley <creechley@gmail.com>
2020-10-05 02:20:08 -0600

    more cleanups - todos

--- tests/tspi.nim
+++ tests/tspi.nim
@@ -13,6 +13,8 @@ var dev: spi_device_handle_t
 
 let trn1 = dev.newSpiTrans([1'u8, 2'u8, 3'u8])
 let trn2 = dev.newSpiTrans([1'u8, 2'u8, 3'u8, 4'u8, 5'u8])
+let trn3 = dev.newSpiTrans(@[1'u8, 2'u8, 3'u8])
 
 echo "trn1: " & repr(trn1)
 echo "trn2: " & repr(trn2)
+echo "trn3: " & repr(trn3)
