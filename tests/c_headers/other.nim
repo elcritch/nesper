f87fc02b519ebaa8f60e970c4800873ca2f80ea2
Jaremy Creechley <creechley@gmail.com>
2020-10-08 23:33:39 -0600

    more proc updates

--- tests/c_headers/other.nim
+++
@@ -1,2 +0,0 @@
-type
-  system_event_handler_t* = proc (event: ptr system_event_t): esp_err_t {.cdecl.}
--- tests/c_headers/spi_types.nim
+++
@@ -1,6 +0,0 @@
-type
-  spi_host_device_t* = enum
-    SPI1_HOST = 0,              ## /< SPI1
-    SPI2_HOST = 1,              ## /< SPI2
-    SPI3_HOST = 2               ## /< SPI3
-
