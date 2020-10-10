ea75b62905752a34a5f2e381a3b4867f346e970c
Jaremy Creechley <creechley@gmail.com>
2020-10-10 00:22:01 -0600

    setting up rpc server

--- esp-idf-examples/simplewifi-rpc/main/server.nim
+++ esp-idf-examples/simplewifi-rpc/main/server.nim
@@ -12,10 +12,18 @@ type
 # Setup RPC Server #
 # var rt1 = newRpcRouter()
 
-proc run_rpc_server*() =
-    echo "starting rpc server on port 5555"
-    startSocketServer[string](Port(5555), readHandler=echoReadHandler, writeHandler=nil, data="echo: ")
-    # startRpcSocketServer(Port(5555))
+when defined(TcpMsgpackRpcServer):
+  proc run_rpc_server*() =
+      echo "starting rpc server on port 5555"
+      startSocketServer[string](Port(5555), readHandler=echoReadHandler, writeHandler=nil, data="echo: ")
+      # startRpcSocketServer(Port(5555))
+
+
+when defined(TcpEchoServer):
+  proc run_rpc_server*() =
+      echo "starting rpc server on port 5555"
+      startSocketServer[string](Port(5555), readHandler=echoReadHandler, writeHandler=nil, data="echo: ")
+      # startRpcSocketServer(Port(5555))
 
 when isMainModule:
     echo "running server"
--- src/nesper/servers/rpc/rpcsocket.nim
+++ src/nesper/servers/rpc/rpcsocket.nim
@@ -4,76 +4,35 @@ import selectors
 import tables
 import posix
 
-import ../consts
-import ../general
+import ../../consts
+import ../../general
+import ../tcpsocket
 
 export net
 
 const TAG = "socketrpc"
 
-type 
-  ControlServerInfo* = ref object 
-    storage*: NvsObject
-    select*: Selector[int]
-    server*: Socket
-    clients*: ref Table[SocketHandle, Socket]
-    tcpMessages*: seq[string]
-
-  ServerInfo* = ref object 
-    select: Selector[int]
-    server: Socket
-    clients: ref Table[SocketHandle, Socket]
-
-
-proc createServerInfo(server: Socket, selector: Selector[int]): ServerInfo = 
-  result = new(ServerInfo)
-  result.server = server
-  result.select = selector
-  result.clients = newTable[SocketHandle, Socket]()
-
 proc handleWrites(result: ReadyKey, srv: ServerInfo) = 
   raise newException(OSError, "the request to the OS failed")
 
-proc handleReads(srv: var ControlServerInfo, selected: ReadyKey) = 
-  if selected.fd.SocketHandle == srv.server.getFd():
-    var client: Socket = new(Socket)
-    srv.server.accept(client)
+proc rpcMsgPackReadHandler*(srv: TcpServerInfo[string], result: ReadyKey, sourceClient: Socket, data: string) =
+  var sourceFd = selected.fd
+  var sourceClient: Socket = srv.clients[sourceFd.SocketHandle]
 
-    client.getFd().setBlocking(false)
-    srv.select.registerHandle(client.getFd(), {Event.Read}, -1)
-    srv.clients[client.getFd()] = client
-    echo("Server: client connected: ", $client.getFd().int)
+  try:
+    var msg: string = newString(BUFF_SZ)
+    var count = sourceClient.recv(msg, BUFF_SZ)
 
-  elif srv.clients.hasKey(selected.fd.SocketHandle):
-    # echo("server: received socket: ", selected.fd)
-    var sourceFd = selected.fd
-    var sourceClient: Socket = srv.clients[sourceFd.SocketHandle]
-
-    try:
-      var msg: string = newString(BUFF_SZ)
-      var count = sourceClient.recv(msg, BUFF_SZ)
-
-      if count == 0:
-        var client: Socket
-        discard srv.clients.pop(sourceFd.SocketHandle, client)
-        srv.select.unregister(sourceFd)
-        discard posix.close(sourceFd.cint)
-        echo("control server: client disconnected: ", " fd: ", $sourceFd)
-      elif count < 0:
-        srv.clients.del(sourceFd.SocketHandle)
-        srv.select.unregister(sourceFd)
-
-        discard posix.close(sourceFd.cint)
-        echo("control server: client read error: ", $(sourceFd))
-      else:
-        msg.setLen(count)
-        srv.tcpMessages.insert(msg, 0)
-        # echo("server: received from client: `", msg, "`")
-    except TimeoutError:
-      echo("control server: error: socket timeout: ", $sourceClient.getFd().int)
-
-  else:
-    echo("control server: error: unknown socket id: ", $selected.fd.int)
+    if count == 0:
+      raise newException(TcpClientDisconnected, "")
+    elif count < 0:
+      raise newException(TcpClientError, "")
+    else:
+      msg.setLen(count)
+      srv.tcpMessages.insert(msg, 0)
+      # echo("server: received from client: `", msg, "`")
+  except TimeoutError:
+    echo("control server: error: socket timeout: ", $sourceClient.getFd().int)
 
 
 
