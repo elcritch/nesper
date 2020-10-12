5eda1b2d1a5ab8a2bcd9881d0cb6606f8137b159
Jaremy Creechley <creechley@gmail.com>
2020-10-12 12:26:08 -0600

    setting up an rpc where calls are handled on another task

---
+++ src/nesper/servers/rpc/rpcsocket_queue_mpack.nim
@@ -0,0 +1,53 @@
+import nativesockets, net, selectors, tables, posix
+
+import ../../consts
+import ../../general
+import ../tcpsocket
+
+import router
+import json
+import msgpack4nim
+import msgpack4nim/msgpack2json
+
+export tcpsocket, router
+
+const TAG = "socketrpc"
+
+proc rpcMsgPackWriteHandler*(srv: TcpServerInfo[RpcRouter], result: ReadyKey, sourceClient: Socket, rt: RpcRouter) =
+  raise newException(OSError, "the request to the OS failed")
+
+proc rpcMsgPackReadHandler*(srv: TcpServerInfo[RpcRouter], result: ReadyKey, sourceClient: Socket, rt: RpcRouter) =
+
+  try:
+    logd(TAG, "rpc server handler: router: %x", rt.buffer)
+
+    let msg = sourceClient.recv(rt.buffer, -1)
+
+    if msg.len() == 0:
+      raise newException(TcpClientDisconnected, "")
+    else:
+      var rcall = msgpack2json.toJsonNode(msg)
+
+      var res: JsonNode = rt.route( rcall )
+      var rmsg: string = msgpack2json.fromJsonNode(res)
+
+      logd(TAG, "sending to client: %s", $(sourceClient.getFd().int))
+      discard sourceClient.send(addr rmsg[0], rmsg.len)
+
+  except TimeoutError:
+    echo("control server: error: socket timeout: ", $sourceClient.getFd().int)
+
+
+proc startRpcSocketServer*(port: Port; router: var RpcRouter) =
+  logi(TAG, "starting mpack rpc server: buffer: %s", $router.buffer)
+
+  startSocketServer[RpcRouter](
+    Port(5555),
+    readHandler=rpcMsgPackReadHandler,
+    writeHandler=rpcMsgPackWriteHandler,
+    data=router)
+
+
+
+when isMainModule:
+    runTcpServer()
