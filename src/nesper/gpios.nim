fb752bf98cb8979fb65f92d91a55643e4019b75b
Jaremy Creechley <creechley@gmail.com>
2020-10-14 14:55:37 -0600

    fixing gpio configs

--- src/nesper/gpios.nim
+++ src/nesper/gpios.nim
@@ -10,21 +10,23 @@ type
   GpioError* = object of OSError
     code*: esp_err_t
 
-proc setupGpios(pins: set[gpio_num_t]) =
+proc config(pins: set[gpio_num_t],
+    mode: gpio_mode_t, ## !< GPIO mode: set input/output mode
+    pull_up_en: gpio_pullup_t = GPIO_PULLUP_DISABLE, ## !< GPIO pull-up
+    pull_down_en: gpio_pulldown_t = GPIO_PULLDOWN_DISABLE, ## !< GPIO pull-down
+    intr_type: gpio_int_type_t = GPIO_INTR_DISABLE, ## !< GPIO interrupt type
+) =
   var pin_mask: uint64 
   for pin in pins:
     pin_mask = pin_mask or BIT(pin.int())
 
   var io_conf: gpio_config_t
-  io_conf.intr_type = GPIO_INTR_DISABLE
-  ## set as output mode
-  io_conf.mode = GPIO_MODE_OUTPUT
+  io_conf.intr_type = intr_type
+  io_conf.mode = mode
   io_conf.pin_bit_mask = pin_mask
-  ## disable pull-down mode
-  io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE
-  ## disable pull-up mode
-  io_conf.pull_up_en = GPIO_PULLUP_DISABLE
-  ## configure GPIO with the given settings
+  io_conf.pull_down_en = pull_down_en
+  io_conf.pull_up_en = pull_up_en
+  
   var ret: esp_err_t
   
   ret = gpio_config(addr(io_conf))
