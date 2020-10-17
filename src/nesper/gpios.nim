0c2cd76c479fa548a911661f5ae6a1042756e628
Jaremy Creechley <creechley@gmail.com>
2020-10-17 01:01:15 -0600

    hal pins setup

--- src/nesper/gpios.nim
+++ src/nesper/gpios.nim
@@ -44,8 +44,7 @@ proc setLevel*(pin: gpio_num_t, value: uint32) =
 
 proc setLevel*(pin: gpio_num_t, value: bool) =
   setLevel(pin, uint32(value))
-
-proc setLevel*(pin: gpio_num_t, value: range[0..1]) =
+proc setLevel*(pin: gpio_num_t, value: SomeInteger) =
   setLevel(pin, uint32(value))
 
 proc getLevel*(pin: gpio_num_t): bool =
