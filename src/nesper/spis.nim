8aa046d6a3517e7fe0d4fb13a79a16ae574df4c1
Jaremy Creechley <creechley@gmail.com>
2020-10-21 01:59:35 -0600

    spi fixes

--- src/nesper/spis.nim
+++ src/nesper/spis.nim
@@ -329,10 +329,10 @@ proc retrieve*(dev: SpiDev, ticks_to_wait: TickType_t = portMAX_DELAY): SpiTrans
   if (ret != ESP_OK):
     raise newSpiError("start polling (" & $esp_err_to_name(ret) & ")", ret)
 
-proc transmit*(trn: SpiTrans): SpiTrans {.inline.} = 
-  result = new(SpiTrans)
+proc transmit*(trn: SpiTrans) {.inline.} = 
+  # result = new(SpiTrans)
   let ret: esp_err_t =
-    spi_device_transmit(trn.dev.handle, addr(result.trn))
+    spi_device_transmit(trn.dev.handle, addr(trn.trn))
 
   if (ret != ESP_OK):
     raise newSpiError("start polling (" & $esp_err_to_name(ret) & ")", ret)
