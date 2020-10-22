5e2431a58f4de5be42cd8d9ef77de69831f09235
Jaremy Creechley <creechley@gmail.com>
2020-10-22 15:20:39 -0600

    adding heap info items

--- src/nesper/general.nim
+++ src/nesper/general.nim
@@ -84,3 +84,22 @@ proc joinBytes64*[T](bs: openArray[byte], count: range[1..8], top=false): T =
     n = n shl (64-N*8)
   return cast[T](n)
 
+
+type 
+  MallocCapacity = enum
+    MALLOC_CAP_EXEC           =  BIT(0)  # ///< Memory must be able to run executable code
+    MALLOC_CAP_32BIT          =  BIT(1)  # ///< Memory must allow for aligned 32-bit data accesses
+    MALLOC_CAP_8BIT           =  BIT(2)  # ///< Memory must allow for 8/16/...-bit data accesses
+    MALLOC_CAP_DMA            =  BIT(3)  # ///< Memory must be able to accessed by DMA
+    MALLOC_CAP_PID2           =  BIT(4)  # ///< Memory must be mapped to PID2 memory space (PIDs are not currently used)
+    MALLOC_CAP_PID3           =  BIT(5)  # ///< Memory must be mapped to PID3 memory space (PIDs are not currently used)
+    MALLOC_CAP_PID4           =  BIT(6)  # ///< Memory must be mapped to PID4 memory space (PIDs are not currently used)
+    MALLOC_CAP_PID5           =  BIT(7)  # ///< Memory must be mapped to PID5 memory space (PIDs are not currently used)
+    MALLOC_CAP_PID6           =  BIT(8)  # ///< Memory must be mapped to PID6 memory space (PIDs are not currently used)
+    MALLOC_CAP_PID7           =  BIT(9)  # ///< Memory must be mapped to PID7 memory space (PIDs are not currently used)
+    MALLOC_CAP_SPIRAM         =  BIT(10) # ///< Memory must be in SPI RAM
+    MALLOC_CAP_INTERNAL       =  BIT(11) # ///< Memory must be internal; specifically it should not disappear when flash/spiram cache is switched off
+    MALLOC_CAP_DEFAULT        =  BIT(12) # ///< Memory can be returned in a non-capability-specific memory allocation (e.g. malloc(), calloc()) call
+    MALLOC_CAP_INVALID        =  BIT(31) # ///< Memory can't be used / list end marker
+
+proc  heap_caps_print_heap_info*(cap: MallocCapacity) {.importc: "$1", header: "heap/include/esp_heap_caps.h".}
