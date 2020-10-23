feafd66ce8e6877e5e1cecd8300882fe56a06c4e
Jaremy Creechley <creechley@gmail.com>
2020-10-23 13:40:40 -0600

    figuring out defaults... wrapping macros

--- src/nesper/esp/net/esp_eth_netif_glue.nim
+++ src/nesper/esp/net/esp_eth_netif_glue.nim
@@ -12,8 +12,8 @@
 ##  See the License for the specific language governing permissions and
 ##  limitations under the License.
 
-import
-  esp_eth
+import ../../consts
+import esp_eth
 
 ## *
 ##  @brief Create a netif glue for Ethernet driver
