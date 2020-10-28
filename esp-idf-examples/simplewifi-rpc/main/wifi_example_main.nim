5dff27a996a8667b67639a62d11ba6339b9f455c
Jaremy Creechley <creechley@gmail.com>
2020-10-28 10:58:36 -0600

    fixing build and add makefile helpers

--- esp-idf-examples/simplewifi-rpc/main/wifi_example_main.nim
+++ esp-idf-examples/simplewifi-rpc/main/wifi_example_main.nim
@@ -5,9 +5,9 @@ import nesper/events
 import nesper/wifi
 import nesper/nvs
 import nesper/tasks
+import os
 
 import server
-import apps
 
 # Get Password
 const WIFI_SSID {.strdefine.}: string = "NOSSID"
@@ -15,9 +15,6 @@ const WIFI_PASSWORD  {.strdefine.}: string = ""
 
 # const CONFIG_EXAMPLE_WIFI_SSID = getEnv("WIFI_SSID")
 # const CONFIG_EXAMPLE_WIFI_PASSWORD = getEnv("WIFI_PASSWORD")
-# {.emit: """
-# volatile void *apploop = NULL;
-# """.}
 
 const
   GOT_IPV4_BIT* = EventBits_t(BIT(1))
@@ -28,86 +25,31 @@ var sConnectEventGroup*: EventGroupHandle_t
 var sIpAddr*: IpAddress
 var sConnectionName*: cstring
 
-# Main App Functions
-proc networkConnect(): esp_err_t
-proc networkDisconnect(): esp_err_t
-proc wifiStop()
-proc wifiStart()
-
-
-app_main():
-  ## Main app entry - callback from `general.nim` that sets up Nim runtime.
-  
-  # Standard wifi setup
-  initNvs()
-  tcpip_adapter_init()
-
-  # Create the default event loop, mostly for network/wifi events
-  # the `check: ` macro is a nickname for the `ESP_ERROR_CHECK` C macro
-  check: esp_event_loop_create_default()
-
-  logi(TAG, "wifi setup!\n")
-  check: networkConnect()
-
-  echo("app done\n")
-
-proc networkConnect(): esp_err_t =
-  ## Connects the wifi device to the network
-  if sConnectEventGroup != nil:
-    return ESP_ERR_INVALID_STATE
-
-  sConnectEventGroup = xEventGroupCreate()
-
-  wifiStart()
-  discard xEventGroupWaitBits(sConnectEventGroup, CONNECTED_BITS, 1, 1, portMAX_DELAY)
-
-  logi(TAG, "Connected to %s", sConnectionName)
-  logi(TAG, "IPv4 address: %s", $sIpAddr)
-
-  # setup and run the rpc socket server on this task
-  echo("run run_rpc_server\n")
-  run_rpc_server()
-
-  return ESP_OK
-
-proc networkDisconnect(): esp_err_t =
-  ## Call to disconnect the wifi device to the network
-  if sConnectEventGroup == nil:
-    return ESP_ERR_INVALID_STATE
-
-  vEventGroupDelete(sConnectEventGroup)
-  sConnectEventGroup = nil
-  wifiStop()
-  logi(TAG, "Disconnected from %s", sConnectionName)
-  sConnectionName = nil
-
-  return ESP_OK
-
-proc ipReceivedHandler(arg: pointer; event_base: esp_event_base_t; event_id: int32;
-               event_data: pointer) {.cdecl.} =
+proc ipReceivedHandler*(arg: pointer; event_base: esp_event_base_t; event_id: int32;
+              event_data: pointer) {.cdecl.} =
   var event: ptr ip_event_got_ip_t = cast[ptr ip_event_got_ip_t](event_data)
-
   logi TAG, "event.ip_info.ip: %s", $(event.ip_info.ip)
 
   sIpAddr = toIpAddress(event.ip_info.ip)
+  # memcpy(addr(sIpAddr), addr(event.ip_info.ip), sizeof((sIpAddr)))
   logw TAG, "got event ip: %s", $sIpAddr
   discard xEventGroupSetBits(sConnectEventGroup, GOT_IPV4_BIT)
 
-proc onWifiDisconnect(arg: pointer;
+proc onWifiDisconnect*(arg: pointer;
                           event_base: esp_event_base_t;
                           event_id: int32;
                           event_data: pointer) {.cdecl.} =
   logi(TAG, "Wi-Fi disconnected, trying to reconnect...")
   check: esp_wifi_connect()
 
-proc wifiStart() =
-  ## Set up Wifi connection details once wifi device is ready
+proc wifiStart*() =
+  ##  set up connection, Wi-Fi or Ethernet
   let wcfg: wifi_init_config_t = wifi_init_config_default()
 
   discard esp_wifi_init(unsafeAddr(wcfg))
 
-  WIFI_EVENT_STA_DISCONNECTED.eventRegister(onWifiDisconnect)
-  IP_EVENT_STA_GOT_IP.eventRegister(ipReceivedHandler)
+  WIFI_EVENT_STA_DISCONNECTED.eventRegister(onWifiDisconnect, nil)
+  IP_EVENT_STA_GOT_IP.eventRegister(ipReceivedHandler, nil)
 
   check: esp_wifi_set_storage(WIFI_STORAGE_RAM)
 
@@ -123,10 +65,62 @@ proc wifiStart() =
 
   sConnectionName = WIFI_SSID 
 
-proc wifiStop() =
-  ##  tear down wifi connection, release resources
+proc wifiStop*() =
+  ##  tear down connection, release resources
   WIFI_EVENT_STA_DISCONNECTED.eventUnregister(onWifiDisconnect) 
   IP_EVENT_STA_GOT_IP.eventUnregister(ipReceivedHandler)
 
   check: esp_wifiStop()
   check: esp_wifi_deinit()
+
+proc exampleConnect*(): esp_err_t =
+  if sConnectEventGroup != nil:
+    return ESP_ERR_INVALID_STATE
+
+  sConnectEventGroup = xEventGroupCreate()
+
+  wifiStart()
+  discard xEventGroupWaitBits(sConnectEventGroup, CONNECTED_BITS, 1, 1, portMAX_DELAY)
+
+  logi(TAG, "Connected to %s", sConnectionName)
+  logi(TAG, "IPv4 address: %s", $sIpAddr)
+
+  echo("run_http_server\n")
+  run_rpc_server()
+
+  return ESP_OK
+
+proc exampleDisconnect*(): esp_err_t =
+  if sConnectEventGroup == nil:
+    return ESP_ERR_INVALID_STATE
+
+  vEventGroupDelete(sConnectEventGroup)
+  sConnectEventGroup = nil
+  wifiStop()
+  logi(TAG, "Disconnected from %s", sConnectionName)
+  sConnectionName = nil
+
+  return ESP_OK
+
+app_main():
+  initNvs()
+  when defined(ESP_IDF_V4_0):
+    tcpip_adapter_init()
+  else:
+    # Initialize TCP/IP network interface (should be called only once in application)
+    check: esp_netif_init()
+
+
+  # Create default event loop that running in background
+  check: esp_event_loop_create_default()
+
+  logi(TAG, "wifi setup!\n")
+  check: exampleConnect()
+
+  ##  Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+  ##  and re-start it upon connection.
+  ##
+  # IP_EVENT_STA_GOT_IP.eventRegister(ipReceivedHandler, nil)
+
+  echo("Wait done\n")
+  # vTaskDelay(10000 div portTICK_PERIOD_MS)
