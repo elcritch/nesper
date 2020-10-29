1d8eba9b4117b1aff9dc0e457985f9606e596930
Jaremy Creechley <creechley@gmail.com>
2020-10-29 12:59:57 -0600

    tweaking json rpc tests

--- src/nesper/servers/rpc/compile_rpc_tests.sh
+++ src/nesper/servers/rpc/compile_rpc_tests.sh
@@ -2,5 +2,5 @@
 
 rm -Rf rpcsocket_json_cache
 
-nim c --cincludes:../../../../tests/c_headers/mock --nimCache:rpcsocket_json_cache/ --gc:arc  --debugger:native --threads:on --tls_emulation:off --verbosity:2 -d:debug rpcsocket_json.nim 
+nim c --cincludes:../../../../tests/c_headers/mock --nimCache:rpcsocket_json_cache/ --gc:arc -d:useMalloc --debugger:native --threads:on --tls_emulation:off --verbosity:2 -d:debug rpcsocket_json.nim 
 
