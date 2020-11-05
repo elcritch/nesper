e7d800b528059dd2ae0a536083febea7a9d15dc4
Jaremy Creechley <creechley@gmail.com>
2020-11-05 09:56:56 -0700

    experiment - change to full nvs partition setup

--- src/nesper/esp/nvs.nim
+++ src/nesper/esp/nvs.nim
@@ -104,7 +104,7 @@ type
 type
 #   nvs_opaque_iterator_t {.header: "<nvs.h>", importc: "nvs_opaque_iterator_t".} = object
 #   nvs_iterator_t* = ptr nvs_opaque_iterator_t
-  nvs_iterator_t {.header: "<nvs.h>", importc: "nvs_iterator_t".} = ptr
+  nvs_iterator_t* {.header: "<nvs.h>", importc: "nvs_iterator_t".} = ptr
 
 ## *
 ##  @brief      Open non-volatile storage with a given namespace from the default NVS partition
