3080c65d8dab613b9c254e231b7bab0181be8f34
Jaremy Creechley <creechley@gmail.com>
2020-11-30 22:32:51 -0700

    copying more ota setup

--- src/nesper/ota_utils.nim
+++ src/nesper/ota_utils.nim
@@ -20,16 +20,16 @@ type
     code*: esp_err_t
 
   OtaUpdateStatus* = enum
-    UpdateInvalidLength,
-    UpdateOk,
-    UpdateMatchesPreviousInvalid
-    UpdateSameAsCurrent
+    VersionNewer,
+    VersionUnchecked,
+    VersionMatchesPreviousInvalid
+    VersionSameAsCurrent
 
 proc `=destroy`(ota: var typeof(OtaUpdateHandle()[])) =
-  # TAG.logi("i2c port finalize")
-  let err = esp_ota_end(ota.handle)
-  if err != ESP_OK:
-    raise newEspError[OtaError]("Error ota end ", err)
+  if ota.handle.uint32 != 0:
+    let err = esp_ota_end(ota.handle)
+    if err != ESP_OK:
+      raise newEspError[OtaError]("Error ota end ", err)
 
 
 proc newOtaUpdateHandle*(startFrom: ptr esp_partition_t = nil): OtaUpdateHandle =
@@ -41,14 +41,27 @@ proc newOtaUpdateHandle*(startFrom: ptr esp_partition_t = nil): OtaUpdateHandle 
   result.running = esp_ota_get_running_partition()
 
 
+proc printPartionInfo*(ota: OtaUpdateHandle) =
+  if ota.configured != ota.running:
+    TAG.logw("Configured OTA boot partition at offset 0x%08x, but running from offset 0x%08x",
+             ota.configured.address, ota.running.address)
+    TAG.logw("(This can happen if either the OTA boot data or preferred boot image become corrupted somehow.)")
+    
+  TAG.logi("Running partition type %d subtype %d (offset 0x%08x)",
+           ota.running.`type`, ota.running.subtype, ota.running.address)
+
+  TAG.logi("Writing to partition subtype %d at offset 0x%x",
+           ota.update.subtype, ota.update.address)
+
+
 proc begin*(ota: OtaUpdateHandle) =
   let err = esp_ota_begin(ota.update, OTA_SIZE_UNKNOWN, addr(ota.handle))
   if err != ESP_OK:
     TAG.loge("esp_ota_begin failed (%s)", repr(ota.update))
     raise newEspError[OtaError]("Error ota begin: %s" & $esp_err_to_name(err), err)
 
-proc checkImageHeader*(ota: OtaUpdateHandle, data: var string):
-      tuple[status: OtaUpdateStatus, desc: esp_app_desc_t] =
+proc checkImageHeader*(ota: OtaUpdateHandle, data: var string; version_check = true):
+      tuple[status: OtaUpdateStatus, info: esp_app_desc_t] =
 
     var new_app_info: esp_app_desc_t
     let
@@ -59,7 +72,7 @@ proc checkImageHeader*(ota: OtaUpdateHandle, data: var string):
     let header_sz = sz_image_hdr + sz_image_seg_hdr + sz_app_desc
 
     if data.len() < header_sz:
-      raise newException(ValueError, "received package is not fit len")
+      raise newException(ValueError, "insufficient update image size: " & $data.len() & "/" & $header_sz)
 
     ##  check current version with downloading
     copyMem(addr new_app_info,
@@ -67,10 +80,12 @@ proc checkImageHeader*(ota: OtaUpdateHandle, data: var string):
             sizeof(esp_app_desc_t))
 
     TAG.logi("New firmware version: %s", new_app_info.version)
+    if not version_check:
+      return (status: VersionUnchecked, info: new_app_info)
 
     var running_app_info: esp_app_desc_t
 
-    if esp_ota_get_partition_description(ota.running, addr(running_app_info)) == ESP_OK:
+    if ota.running.esp_ota_get_partition_description(addr running_app_info) == ESP_OK:
       TAG.logi("Running firmware version: %s", running_app_info.version)
 
     var last_invalid_app: ptr esp_partition_t = esp_ota_get_last_invalid_partition()
@@ -84,6 +99,9 @@ proc checkImageHeader*(ota: OtaUpdateHandle, data: var string):
       TAG.logw("Previously, there was an attempt to launch the firmware with %s version, but it failed.",
                 invalid_app_info.version)
       TAG.logw("The firmware has been rolled back to the previous version.")
+      return (status: VersionMatchesPreviousInvalid, info: new_app_info)
 
     if new_app_info.version == running_app_info.version:
-      raise newException(OtaError, "Current running version is the same as a new.")
+      return (status: VersionSameAsCurrent, info: new_app_info)
+
+    return (status: VersionNewer, info: new_app_info)
