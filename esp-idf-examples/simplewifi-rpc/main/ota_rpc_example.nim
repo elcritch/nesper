866feda114c3f200fd40029e5fca6f820e65e902
Jaremy Creechley <creechley@gmail.com>
2020-12-01 18:20:32 -0700

    adding example otc methods

---
+++ esp-idf-examples/simplewifi-rpc/main/ota_rpc_example.nim
@@ -0,0 +1,61 @@
+import nesper
+import nesper/ota_utils
+import nesper/servers/rpc/rpcsocket_mpack
+
+import json, options
+import std/sha1
+
+const TAG = "OTARPC"
+
+# This needs to be global, or it could be a thread local
+var ota: OtaUpdateHandle = nil
+
+
+proc checkSha1Hash*(val: string, hash: string) =
+    let sh = $secureHash(val)
+    if sh != hash:
+      raise newException(ValueError, "incorrect hash")
+
+proc addOTAMethods*(rt: var RpcRouter) =
+
+  rpc(rt, "firmware-begin") do(img_head: string, sha1_hash: string) -> JsonNode:
+    if ota != nil:
+      raise newException(ValueError, "firmware update already started! reboot esp")
+
+    img_head.checkSha1Hash(sha1_hash)
+
+    ota = newOtaUpdateHandle(nil)
+    let desc = ota.checkImageHeader(img_head, version_check = true)
+    # tuple[status: OtaUpdateStatus, info: esp_app_desc_t]
+    case desc.status:
+    of VersionUnchecked, VersionMatchesPreviousInvalid, VersionSameAsCurrent:
+      result = % ["incorrect", $desc.status]
+    of VersionNewer:
+      result = % ["ok", $desc.status]
+
+    ota.begin()
+    ota.write(img_head)
+
+  rpc(rt, "firmware-chunk") do(img_chunk: string, sha1_hash: string) -> int:
+    img_chunk.checkSha1Hash(sha1_hash)
+    ota.write(img_chunk)
+    TAG.logi("wrote image chunk: size: %d", img_chunk.len())
+    result = ota.total_written
+
+  rpc(rt, "firmware-finish") do(sha1_hash: string) -> int:
+    # todo: final check of written data with sha1_hash?
+    # it seems the ota library does a sha256 check itself
+    TAG.logw("finalize: firmware finish %s", sha1_hash)
+    ota.finish()
+    delayMillis(10)
+    TAG.logw("finalize: write finished",)
+    ota.set_as_boot_partition()
+    TAG.logw("finalize: set as boot",)
+    result = ota.total_written
+    ota = nil
+    return 0
+
+  rpc(rt, "firmware-verify") do() -> string:
+    firmware_verify(proc (): bool = true)
+
+    return "ok"
--- esp-idf-examples/simplewifi-rpc/main/server.nim
+++ esp-idf-examples/simplewifi-rpc/main/server.nim
@@ -5,17 +5,24 @@ import apps
 import volatile
 import strutils
 import json
-# import nesper/servers/rpc/socketrpc
 
 const TAG = "server"
 const MaxRpcReceiveBuffer {.intdefine.}: int = 4096
 
+## Note:
+## Nim uses `when` compile time constructs
+## these are like ifdef's in C and don't really have an equivalent in Python
+## setting the flags can be done in the Makefile `simplewifi-rpc  Makefile
+## for example, to compile the example to use JSON, pass `-d:TcpJsonRpcServer` to Nim
+## the makefile has several example already defined for convenience
+## 
 when defined(TcpJsonRpcServer):
   import nesper/servers/rpc/rpcsocket_json
-  # when defined(TcpMpackRpcServer):
 when defined(TcpMpackRpcQueueServer):
+  import ota_rpc_example
   import nesper/servers/rpc/rpcsocket_queue_mpack
-elif not defined(TcpEchoServer):
+elif not defined(TcpEchoServer): # This is the default 'msgpack' version -- set like this for nimsuggest
+  import ota_rpc_example
   import nesper/servers/rpc/rpcsocket_mpack
 elif defined(TcpEchoServer):
   import nesper/servers/tcpsocket
@@ -34,32 +41,40 @@ else:
   # Setup RPC Server #
   proc run_rpc_server*() =
 
+    # Setup an RPC router
     var rpcRouter: RpcRouter 
-
-    # Setup an app task apploop
-    # note: not sure if this is the best place for it or not?
     var rt = createRpcRouter(MaxRpcReceiveBuffer)
 
     rpc(rt, "hello") do(input: string) -> string:
+      # example: ./rpc_cli --ip:$IP -c:1 '{"method": "hello", "params": ["world"]}'
       result = "Hello " & input
 
     rpc(rt, "add") do(a: int, b: int) -> int:
+      # example: ./rpc_cli --ip:$IP -c:1 '{"method": "add", "params": [1, 2]}'
       result = a + b
 
     rpc(rt, "addAll") do(vals: seq[int]) -> int:
-
+      # example: ./rpc_cli --ip:$IP -c:1 '{"method": "add", "params": [1, 2, 3, 4, 5]}'
       echo("run_rpc_server: done: " & repr(addr(vals)))
       result = 0
       for x in vals:
         result += x
 
-    when not defined(TcpMpackRpcQueueServer):
-      echo "starting rpc server on port 5555"
-      logi(TAG,"starting rpc server buffer size: %s", $(rt.buffer))
-      startRpcSocketServer(Port(5555), router=rt)
-    else:
+    # this adds methods for Over-The-Air updates using RPC! 
+    # note, this isn't secured by default
+    rpcRouter.addOTAMethods()
 
+    echo "starting rpc server on port 5555"
+    logi(TAG,"starting rpc server buffer size: %s", $(rt.buffer))
+
+    when defined(TcpMpackRpcQueueServer):
+      # Starts a separate task on CPU_1 (e.g. the "App CPU")
+      # This makes it reallly easy to add dedicated tasks to run on CPU_1
       startRpcQueueSocketServer(Port(5555), router=rt) 
+    else:
+      # Starts RPC handler on the current task 
+      # Default for JSON, MsgPack, and TCP Echo
+      startRpcSocketServer(Port(5555), router=rt)
 
 
 
