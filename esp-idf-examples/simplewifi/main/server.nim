615026f1ff5eb03946a3eee76e79c3bc93268c1e
Jaremy Creechley <creechley@gmail.com>
2021-01-11 00:02:57 -0700

    adding esp timer test

--- esp-idf-examples/simplewifi/main/server.nim
+++ esp-idf-examples/simplewifi/main/server.nim
@@ -15,7 +15,28 @@ proc run_http_server*() {.exportc.} =
 
     waitFor server.serve(Port(8181), cb)
 
+import nesper/esp/esp_timer
+
+proc example_cb*(arg: pointer) {.cdecl.} = 
+  echo "Done!"
+
+proc timer_test*(arg: pointer) {.cdecl.} = 
+    var x = "hello"
+
+    var timer_handle = esp_timer_create_args_t(
+    callback: example_cb,
+    arg: x.cstring,
+    dispatch_method: ESP_TIMER_TASK,
+    name: "timer1")
+
+    var timer1: esp_timer_handle_t
+
+    discard esp_timer_create(addr timer_handle, addr timer1)
+    discard esp_timer_start_periodic(timer1, 1000.uint64)
+
+
 when isMainModule:
     echo "running server"
     run_http_server()
+    timer_test()
 
--- tests/driver/tesp_timer.nim
+++ tests/driver/tesp_timer.nim
@@ -1,21 +1,16 @@
 
-import nesper/esp/esp_timer
+import nesper/timers
 
 proc example_cb*(arg: pointer) {.cdecl.} = 
   echo "Done!"
 
 var x = "hello"
 
-var timer_handle = esp_timer_create_args_t(
-  callback: example_cb,
-  arg: x.cstring,
-  dispatch_method: ESP_TIMER_TASK,
-  name: "timer1")
-
-var
-  timer1: esp_timer_handle_t
-
-discard esp_timer_create(addr timer_handle, addr timer1)
+var timer1 = createTimer(
+  callback= example_cb,
+  arg= x.cstring,
+  dispatch_method= ESP_TIMER_TASK,
+  name= "timer1")
 
 discard esp_timer_start_periodic(timer1, 1000.uint64)
 
