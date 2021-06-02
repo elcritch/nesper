68bb07c15b29b8da093a5a743d89bf8b97589941
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-06-02 12:26:57 -0700

    updating ethernet

--- src/nesper/ethernet.nim
+++ src/nesper/ethernet.nim
@@ -2,7 +2,9 @@
 import nesper/esp/net/esp_eth_com
 import nesper/esp/net/esp_eth_mac
 import nesper/esp/net/esp_eth
+import nesper/esp/net/tcpip_adapter
 
 export esp_eth_com
 export esp_eth_mac
 export esp_eth
+export tcpip_adapter
