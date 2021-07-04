1ea80c4d1d438aa34eab3671ab60df0038d5088f
Jaremy Creechley <creechley@gmail.com>
2021-07-04 01:09:08 -0700

    implementing function

--- src/nesper/esp-idf-lib/sts21.nim
+++ src/nesper/esp-idf-lib/sts21.nim
@@ -19,6 +19,7 @@ type
 proc initSts21*(cfg: I2CDevConfig, clock_hz = Hertz(100_000)): ConfigSTS21 = 
 
   new(result)
+  new(result.device)
   result.device.dev.port = cfg.port
   result.device.dev.devAddr = STS21_ADDR
   result.device.dev.cfg.sda_io_num = cfg.sda
@@ -32,19 +33,24 @@ proc takeReading*(cfg: var ConfigSTS21): float =
   let
     devptr = addr cfg.device.dev
 
-  check: i2c_dev_take_mutex(devptr)
+  # check: i2c_dev_take_mutex(devptr)
   var in_data = @[0xF3'u8]
   check: i2c_dev_write(devptr, nil, 0, addr in_data[0], 1)
-  check: i2c_dev_give_mutex(devptr)
+  # check: i2c_dev_give_mutex(devptr)
 
   delayMillis(300)
 
-  var output = newSeq[byte](2)
-  check: i2c_dev_read(devptr, nil, 0, addr output[0], 2)
+  var data = newSeq[byte](2)
+  check: i2c_dev_read(devptr, nil, 0, addr data[0], 2)
 
-  "TEMP".logi("read: %s", $output)
+  # "TEMP".logi("read: %s", $data)
+  # // Convert the data
+  let
+    rawtmp = data[0] * 256 + data[1]
+    value = float(rawtmp and 0xFFFC)
+    cTemp = -46.85 + (175.72 * (value / 65536.0))
 
-  return 0.0
+  return cTemp
 
 
 when isMainModule:
