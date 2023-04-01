e40d9350c82702a540726f8119dc337d2b239bd2
faldor20 <eli.jambu@yahoo.com>
2023-04-01 15:50:29 +1000

    clean exports
    put back export for compatibility

--- src/nesper.nim
+++ src/nesper.nim
@@ -5,10 +5,19 @@
 import nesper/consts
 import nesper/general
 import nesper/esp/esp_log
+import nesper/gpios
+import nesper/nimy/button
 
 export consts
 export general
 export esp_log
+
+# nimmy wrapper
+import nesper/nimy/errors
+import nesper/nimy/gpio
+
+export errors
+export gpio
 
 #{.emit: """/*TYPESECTION*/
 ##include <freertos/FreeRTOS.h>
