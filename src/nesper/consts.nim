cc25a8bc8a05177e14532cf1744a8bd9c39b3747
Jaremy Creechley <creechley@gmail.com>
2025-05-13 20:42:27 -0600

    print esp idf version used

--- src/nesper/consts.nim
+++ src/nesper/consts.nim
@@ -41,6 +41,8 @@ static:
   if ESP_IDF_VERSION == ESP_IDF_VERSION_VAL(0, 0, 0):
     warning("ESP_IDF_VERSION: " & ESP_IDF_VER)
     error("Must set esp-idf version using `-d:ESP_IDF_VERSION=4.4` or using an environment variable `export ESP_IDF_VERSION=4.4`")
+  else:
+    warning("Using ESP_IDF_VERSION: " & ESP_IDF_VER)
   if ESP_IDF_MAJOR notin [4, 5]:
     error("Incorrect esp-idf major version: " & $ESP_IDF_MAJOR)
 
