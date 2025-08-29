f1d2dabaed7812c6cc54c686df1fda4e2c60b78c
Jaremy Creechley <creechley@gmail.com>
2025-08-29 02:16:41 -0600

    adding ledc test

--- src/nesper/esp/hal/ledc_types.nim
+++ src/nesper/esp/hal/ledc_types.nim
@@ -6,11 +6,11 @@
 
 const hdr = "<hal/ledc_types.h>"
 
-let LEDC_AUTO_CLK {.importc: "LEDC_AUTO_CLK", header: hdr.}: cint                               ##*!< LEDC source clock will be automatically selected based on the giving resolution and duty parameter when init the timer*/
-let LEDC_USE_APB_CLK {.importc: "LEDC_USE_APB_CLK", header: hdr.}: cint             ##*!< Select APB as the source clock */
-let LEDC_USE_RC_FAST_CLK {.importc: "LEDC_USE_RC_FAST_CLK", header: hdr.}: cint     ##*!< Select RC_FAST as the source clock */
-let LEDC_USE_REF_TICK {.importc: "LEDC_USE_REF_TICK", header: hdr.}: cint       ##*!< Select REF_TICK as the source clock */
-let LEDC_USE_RTC8M_CLK {.importc: "LEDC_USE_RTC8M_CLK", header: hdr.}: cint   ##*!< Alias of 'LEDC_USE_RC_FAST_CLK' */
+let LEDC_AUTO_CLK* {.importc: "LEDC_AUTO_CLK", header: hdr.}: cint                               ##*!< LEDC source clock will be automatically selected based on the giving resolution and duty parameter when init the timer*/
+let LEDC_USE_APB_CLK* {.importc: "LEDC_USE_APB_CLK", header: hdr.}: cint             ##*!< Select APB as the source clock */
+let LEDC_USE_RC_FAST_CLK* {.importc: "LEDC_USE_RC_FAST_CLK", header: hdr.}: cint     ##*!< Select RC_FAST as the source clock */
+let LEDC_USE_REF_TICK* {.importc: "LEDC_USE_REF_TICK", header: hdr.}: cint       ##*!< Select REF_TICK as the source clock */
+let LEDC_USE_RTC8M_CLK* {.importc: "LEDC_USE_RTC8M_CLK", header: hdr.}: cint   ##*!< Alias of 'LEDC_USE_RC_FAST_CLK' */
 
 type
   ledc_mode_t* = distinct cint
@@ -44,10 +44,10 @@ when defined(SOC_LEDC_SUPPORTED):
     ledc_slow_clk_sel_t* = distinct cint
   
   let
-    LEDC_SLOW_CLK_RC_FAST {.importc: "LEDC_SLOW_CLK_RC_FAST", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is RC_FAST clock
-    LEDC_SLOW_CLK_APB {.importc: "LEDC_SLOW_CLK_APB", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is 80MHz APB clock
-    LEDC_SLOW_CLK_PLL_DIV {.importc: "LEDC_SLOW_CLK_PLL_DIV", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is a PLL_DIV clock
-    LEDC_SLOW_CLK_XTAL {.importc: "LEDC_SLOW_CLK_XTAL", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source XTAL clock
+    LEDC_SLOW_CLK_RC_FAST* {.importc: "LEDC_SLOW_CLK_RC_FAST", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is RC_FAST clock
+    LEDC_SLOW_CLK_APB* {.importc: "LEDC_SLOW_CLK_APB", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is 80MHz APB clock
+    LEDC_SLOW_CLK_PLL_DIV* {.importc: "LEDC_SLOW_CLK_PLL_DIV", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source is a PLL_DIV clock
+    LEDC_SLOW_CLK_XTAL* {.importc: "LEDC_SLOW_CLK_XTAL", header: hdr.}: ledc_slow_clk_sel_t ## !< LEDC low speed timer clock source XTAL clock
 
   ##
   ##  @brief LEDC clock source configuration struct
