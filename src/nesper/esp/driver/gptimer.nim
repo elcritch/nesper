1e1c2c93ae3a60aad23591a5593a93919ef0ded6
Jaremy Creechley <creechley@gmail.com>
2025-08-31 01:49:02 -0600

    add gpt wrappers

--- src/nesper/esp/driver/gptimer.nim
+++ src/nesper/esp/driver/gptimer.nim
@@ -4,6 +4,39 @@
 ##  SPDX-License-Identifier: Apache-2.0
 ##
 
+import ../../consts
+import ./timer_types
+
+type
+
+  gptimer_t* {.importc: "gptimer_t", header: "gptimer_types.h", bycopy.} = object
+
+  gptimer_handle_t* = ptr gptimer_t ##
+                                    ##  @brief Type of General Purpose Timer handle
+                                    ##
+
+  gptimer_alarm_event_data_t* {.importc: "gptimer_alarm_event_data_t",
+                                header: "gptimer_types.h", bycopy.} = object ##
+                              ##
+                              ##  @brief GPTimer alarm event data
+                              ##
+    count_value* {.importc: "count_value".}: uint64 ##
+                              ## !< Current count value
+    alarm_value* {.importc: "alarm_value".}: uint64
+    ## !< Current alarm value
+
+
+  gptimer_alarm_cb_t* = proc (timer: gptimer_handle_t;
+                              edata: ptr gptimer_alarm_event_data_t;
+                              user_ctx: pointer): bool {.cdecl.} ##
+                              ##
+                              ##  @brief Timer alarm callback prototype
+                              ##
+                              ##  @param[in] timer Timer handle created by `gptimer_new_timer`
+                              ##  @param[in] edata Alarm event data, fed by driver
+                              ##  @param[in] user_ctx User data, passed from `gptimer_register_event_callbacks`
+                              ##  @return Whether a high priority task has been waken up by this function
+                              ##
 type
 
   INNER_C_STRUCT_gptimer_1* {.importc: "gptimer_config_t::no_name",
@@ -298,3 +331,62 @@ proc gptimer_stop*(timer: gptimer_handle_t): esp_err_t {.cdecl,
                                                   ##       - ESP_ERR_INVALID_STATE: Stop GPTimer failed because the timer is not in running.
                                                   ##       - ESP_FAIL: Stop GPTimer failed because of other error
                                                   ## 
+
+##
+##  @brief GPTimer ETM event configuration
+##
+type
+
+  gptimer_etm_event_config_t* {.importc: "gptimer_etm_event_config_t",
+                                header: "gptimer_etm.h", bycopy.} = object
+    event_type* {.importc: "event_type".}: gptimer_etm_event_type_t
+    ## !< GPTimer ETM event type
+
+
+proc gptimer_new_etm_event*(timer: gptimer_handle_t;
+                            config: ptr gptimer_etm_event_config_t;
+                            out_event: ptr esp_etm_event_handle_t): esp_err_t {.
+    cdecl, importc: "gptimer_new_etm_event", header: "gptimer_etm.h".}
+  ##
+                            ##
+                            ##  @brief Get the ETM event for GPTimer
+                            ##
+                            ##  @note The created ETM event object can be deleted later by calling `esp_etm_del_event`
+                            ##
+                            ##  @param[in] timer Timer handle created by `gptimer_new_timer`
+                            ##  @param[in] config GPTimer ETM event configuration
+                            ##  @param[out] out_event Returned ETM event handle
+                            ##  @return
+                            ##       - ESP_OK: Get ETM event successfully
+                            ##       - ESP_ERR_INVALID_ARG: Get ETM event failed because of invalid argument
+                            ##       - ESP_FAIL: Get ETM event failed because of other error
+                            ##
+##
+##  @brief GPTimer ETM task configuration
+##
+type
+
+  gptimer_etm_task_config_t* {.importc: "gptimer_etm_task_config_t",
+                                header: "gptimer_etm.h", bycopy.} = object
+    task_type* {.importc: "task_type".}: gptimer_etm_task_type_t
+    ## !< GPTimer ETM task type
+
+
+proc gptimer_new_etm_task*(timer: gptimer_handle_t;
+                            config: ptr gptimer_etm_task_config_t;
+                            out_task: ptr esp_etm_task_handle_t): esp_err_t {.
+    cdecl, importc: "gptimer_new_etm_task", header: "gptimer_etm.h".}
+  ##
+                            ##
+                            ##  @brief Get the ETM task for GPTimer
+                            ##
+                            ##  @note The created ETM task object can be deleted later by calling `esp_etm_del_task`
+                            ##
+                            ##  @param[in] timer Timer handle created by `gptimer_new_timer`
+                            ##  @param[in] config GPTimer ETM task configuration
+                            ##  @param[out] out_task Returned ETM task handle
+                            ##  @return
+                            ##       - ESP_OK: Get ETM task successfully
+                            ##       - ESP_ERR_INVALID_ARG: Get ETM task failed because of invalid argument
+                            ##       - ESP_FAIL: Get ETM task failed because of other error
+
