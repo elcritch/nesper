37e8514c97f455279319e3afa89a09f15e6a28ee
Jaremy Creechley <creechley@gmail.com>
2025-08-31 03:42:09 -0600

    tasks

--- tests/thello_world.nim
+++ tests/thello_world.nim
@@ -1,33 +1,3 @@
-import std/strutils
-import nesper
-import nesper/[consts, general, timers]
-import nesper/esp/esp_system
+import nesper/build_utils/templates/hello_world
 
-const
-  TAG*: cstring = "main"
-
-app_main:
-  logi(TAG, "esp starting ... ")
-
-  # Print chip information
-  var chipInfo: esp_chip_info_t
-  esp_chip_info(addr chipInfo)
-  var features = ""
-  try:
-    features &= "This is a " & $chipInfo.model 
-    features &= " chip with " & $chipInfo.cores & " CPU core(s)"
-    if (chipInfo.features and CHIP_FEATURE_WIFI_BGN) != 0:
-      features &= "WiFi/"
-    if (chipInfo.features and CHIP_FEATURE_BT) != 0:
-      features &= "BT"
-    if (chipInfo.features and CHIP_FEATURE_BLE) != 0:
-      features &= "BLE"
-    if (chipInfo.features and CHIP_FEATURE_IEEE802154) != 0:
-      features &= "802.15.4 (Zigbee/Thread)"
-    logi(TAG, "chip features: %s", features.cstring)
-  except Defect, CatchableError:
-    logi(TAG, "Error getting chip info")
-
-  while true:
-    logi(TAG, "Hello, World!")
-    delay(1_000.Millis)
+export hello_world
--- tests/thwlloe_world.nim
+++
@@ -1,3 +0,0 @@
-import nesper/build_utils/templates/hello_world
-
-export hello_world
