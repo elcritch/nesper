e20e12763f1baf5c51427a532e4480344794c3b8
Jaremy Creechley <creechley@gmail.com>
2025-08-31 23:01:57 -0600

    usb

---
+++ src/nesper/components/esp_tinyusb/tusb_tasks.nim
@@ -0,0 +1,31 @@
+##
+##  SPDX-FileCopyrightText: 2020-2022 Espressif Systems (Shanghai) CO LTD
+##
+##  SPDX-License-Identifier: Apache-2.0
+##
+
+
+proc tusb_run_task*(): esp_err_t {.cdecl, importc: "tusb_run_task",
+                                   header: "tusb_tasks.h".}
+  ##
+                              ##
+                              ##  @brief This helper function creates and starts a task which wraps `tud_task()`.
+                              ##
+                              ##  The wrapper function basically wraps tud_task and some log.
+                              ##  Default parameters: stack size and priority as configured, argument = NULL, not pinned to any core.
+                              ##  If you have more requirements for this task, you can create your own task which calls tud_task as the last step.
+                              ##
+                              ##  @retval ESP_OK run tinyusb main task successfully
+                              ##  @retval ESP_FAIL run tinyusb main task failed of internal error
+                              ##  @retval ESP_ERR_INVALID_STATE tinyusb main task has been created before
+                              ##
+
+proc tusb_stop_task*(): esp_err_t {.cdecl, importc: "tusb_stop_task",
+                                    header: "tusb_tasks.h".}
+  ##
+                              ##
+                              ##  @brief This helper function stops and destroys the task created by `tusb_run_task()`
+                              ##
+                              ##  @retval ESP_OK stop and destory tinyusb main task successfully
+                              ##  @retval ESP_ERR_INVALID_STATE tinyusb main task hasn't been created yet
+                              ## 
