c0dcf3b86b3d9d5a688f717549a4601c7b008367
Jaremy Creechley <creechley@gmail.com>
2025-09-03 00:09:23 -0600

    cleanup

--- esp-idf-examples/net_pppos_usb/main/main.nim
+++ esp-idf-examples/net_pppos_usb/main/main.nim
@@ -9,6 +9,7 @@ import nesper/esp/net/esp_netif_ppp
 import nesper/esp/net/esp_netif_impl
 import nesper/net_utils
 
+import nesper/servers/rpc/rpcsocket_json
 
 when defined(RUN_NCM):
   import usb/usb_ncm_eth_dualcdc as ncm_eth
@@ -23,38 +24,14 @@ proc addIpv6ToNetif*(netif: ptr esp_netif_t; ip: IpAddress): esp_err_t =
   let espIp6 = toEspIp6Addr(ip)
   result = esp_netif_add_ip6_address(netif, espIp6, preferred=true)
 
-when defined(RPC_JSON):
-  import nesper/servers/rpc/rpcsocket_json
-  proc setupRpc(rt: var RpcRouter) =
-    rt.rpc("hello") do(input: string) -> string:
-      # example: ./rpc_cli --ip:$IP -c:1 '{"method": "hello", "params": ["world"]}'
-      result = "Hello " & input
+proc setupRpc(rt: var RpcRouter) =
+  rt.rpc("hello") do(input: string) -> string:
+    # example: ./rpc_cli --ip:$IP -c:1 '{"method": "hello", "params": ["world"]}'
+    result = "Hello " & input
 
-    rt.rpc("add") do(a: int, b: int) -> int:
-      # example: ./rpc_cli --ip:$IP -c:1 '{"method": "add", "params": [1, 2]}'
-      result = a + b
-
-import mummy, mummy/routers
-
-proc indexHandler(request: Request) =
-  var headers: HttpHeaders
-  headers["Content-Type"] = "text/plain"
-  request.respond(200, headers, "Hello, World!")
-
-proc runMummy() =
-  var router: Router
-  router.get("/", indexHandler)
-
-  let server = newServer(router,
-                          websocketHandler=nil,
-                          logHandler=nil,
-                          workerThreads=1,
-                          maxHeadersLen=8*1024,
-                          maxBodyLen=128*1024,
-                          maxMessageLen=16*1024
-                          )
-  echo "Serving on http://localhost:8080"
-  server.serve(Port(8080), address="::")
+  rt.rpc("add") do(a: int, b: int) -> int:
+    # example: ./rpc_cli --ip:$IP -c:1 '{"method": "add", "params": [1, 2]}'
+    result = a + b
 
 app_main:
   logi(TAG, "esp starting ... ")
@@ -99,10 +76,10 @@ app_main:
     else:
       loge(TAG, "PPPoS connect failed")
 
-  # var rt: RpcRouter = createRpcRouter(4096)
-  # rt.setupRpc()
+  var rt: RpcRouter = createRpcRouter(4096)
+  rt.setupRpc()
 
-  # startRpcSocketServer(port=Port(5555), address="::", router=rt)
+  startRpcSocketServer(port=Port(5555), address="::", router=rt)
 
   # Keep app alive; clean shutdown not triggered in this minimal example
   while true:
